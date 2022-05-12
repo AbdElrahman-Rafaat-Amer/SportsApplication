3b9e7a6278e6573f4ed5d9264ef38bdbb1e7e49d
AbdElRahman Rafaat Amer <3bdorafaat@gmail.com>
2022-05-12 14:08:32 -0700

    UI Favorite

---
+++ SportsApplication/LeaguesDeatilsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LeaguesDeatilsViewController.swift
+//  SportsApplication
+//
+//  Created by AbdElrahman Amer on 5/12/22.
+//  Copyright © 2022 Shrouk Mamdouh. All rights reserved.
+//
+
+import UIKit
+
+class LeaguesDeatilsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
