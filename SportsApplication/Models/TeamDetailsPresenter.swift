186c29cd6ec867cda0ea0d6b731da59cdb968325
shrouk <shroukmamdouh.mabdelaziz@gmail.com>
2022-05-19 06:50:29 -0700

    Finished team details screen

---
+++ SportsApplication/Models/TeamDetailsPresenter.swift
@@ -0,0 +1,23 @@
+//
+//  TeamDetailsPresenter.swift
+//  SportsApplication
+//
+//  Created by Shrouk Mamdouh on 5/19/22.
+//  Copyright © 2022 Shrouk Mamdouh. All rights reserved.
+//
+
+import Foundation
+
+class TeamDetailsPresenter : TeamDetailsPresenterProtocol{
+    
+   
+    
+    
+    var teamView = TeamDetailsViewController()
+    
+    
+}
+
+protocol TeamDetailsPresenterProtocol {
+   
+}
--- SportsApplication/Module/TeamDetailsScreen/view/TeamDetailsViewController.swift
+++ SportsApplication/Module/TeamDetailsScreen/view/TeamDetailsViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TeamDetailsViewController: UIViewController {
 
+    
+    var myTeam : Team?
+    
+    @IBOutlet weak var stadiumImage: UIImageView!
+    
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    
+    @IBOutlet weak var stadiumName: UILabel!
+  
+    @IBOutlet weak var stadiumLocation: UILabel!
+    
+    
+    @IBOutlet weak var formedYear: UILabel!
+    
+    @IBOutlet weak var leagueName: UILabel!
+    
+    @IBOutlet weak var teamName: UILabel!
+    
+    
+    @IBOutlet weak var countryName: UILabel!
     override func viewDidLoad() {
-        super.viewDidLoad()
+          super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        stadiumName.text = myTeam?.strStadium
+        
+        
+        stadiumLocation.text = myTeam?.strStadiumLocation
+        
+        teamName.text = myTeam?.strTeam
+          
+        formedYear.text = myTeam?.intFormedYear
+        
+        
+        countryName.text = myTeam?.strCountry
+          
+        leagueName.text = myTeam?.strLeague
+        
+        stadiumImage.kf.setImage(with: URL(string: myTeam?.strStadiumThumb ?? ""))
+        
+        logoImage.kf.setImage(with: URL(string: myTeam?.strTeamBadge ?? ""))
+          // Do any additional setup after loading the view.
+      }
     
 
     /*
